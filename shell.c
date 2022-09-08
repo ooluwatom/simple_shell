c0e772af063c8fd96a3c87375aff1cdf45a35431
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2022-09-08 09:27:53 +0100

    gcc checks

--- shell.c
+++ shell.c
@@ -1,50 +1,32 @@
 #include "shell.h"
 /**
-* main - carries out the read, execute then print output loop
-* @ac: argument count
-* @av: argument vector
-* @envp: environment vector
-*
-* Return: 0
-*/
-
-int main(int ac, char **av, char *envp[])
+ * main - Simple Shell
+ * @ac: Argument counter.
+ * @av: Argument values.
+ * @env: Environment variables.
+ *
+ * Return: 0 or -1 in failure.
+ */
+int main(int ac, char **av, char **env)
 {
-	char *line = NULL, *pathcommand = NULL, *path = NULL;
-	size_t bufsize = 0;
-	ssize_t linesize = 0;
-	char **command = NULL, **paths = NULL;
-	(void)envp, (void)av;
-	if (ac < 1)
-		return (-1);
-	signal(SIGINT, handle_signal);
+	/* char *shell_pharse; */
+	command_t **cmd_list = NULL;/* Command List */
+	char *path = NULL;
+	/* history_t **history = malloc(sizeof(history_t)); */
+
+	path = find_path(env);
+	ac++;
+	/* shell_pharse = isatty(STDIN_FILENO) ? "> " : NULL; */
 	while (1)
 	{
-		free_buffers(command);
-		free_buffers(paths);
-		free(pathcommand);
-		prompt_user();
-		linesize = getline(&line, &bufsize, stdin);
-		if (linesize < 0)
-			break;
-		info.ln_count++;
-		if (line[linesize - 1] == '\n')
-			line[linesize - 1] = '\0';
-		command = tokenizer(line);
-		if (command == NULL || *command == NULL || **command == '\0')
-			continue;
-		if (checker(command, line))
-			continue;
-		path = find_path();
-		paths = tokenizer(path);
-		pathcommand = test_path(paths, command[0]);
-		if (!pathcommand)
-			perror(av[0]);
+		cmd_list = _prompt(av[0], av[1]); /* get commands from cmd_line */
+		if (cmd_list)
+		{
+			if (_fork(av[0], *cmd_list, path, env))
+				error_handler(av[0], 102);
+		}
 		else
-			execution(pathcommand, command);
+			error_handler(av[0], 103);
 	}
-	if (linesize < 0 && flags.interactive)
-		write(STDERR_FILENO, "\n", 1);
-	free(line);
 	return (0);
 }
--- shell.h
+++ shell.h
@@ -1,61 +1,103 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdarg.h>
 #include <stdio.h>
+#include <wchar.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <sys/stat.h>
-#include <time.h>
-#include <stdbool.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
-/* environment variables */
-extern char **environ;
-extern __sighandler_t signal(int __sig, __sighandler_t __handler);
+/**
+ * struct error_msg - An structure for each error message
+ *
+ * @ecode: error code
+ * @msg: pointer to error message
+ * @size: error message length.
+ */
+typedef struct error_msg
+{
+	int ecode;
+	char *msg;
+	int  size;
+} error_msg_t;
 
-/* handle built ins */
-int checker(char **cmd, char *buf);
-void prompt_user(void);
-void handle_signal(int m);
-char **tokenizer(char *line);
-char *test_path(char **path, char *command);
-char *append_path(char *path, char *command);
-int handle_builtin(char **command, char *line);
-void exit_cmd(char **command, char *line);
+/**
+ * struct built_s - Builtings commands
+ * @command: command name.
+ * @f: function to call.
+ *
+ * Description: Longer description
+ */
+typedef struct built_s
+{
+	char *command;
+	void (*f)(char **);
+} built_t;
 
-void print_env(void);
+/**
+ * struct history - An structure for each command readed
+ *
+ * @id_h: error code
+ * @comms: Commands
+ * @prev: Previous element
+ * @next: Next element
+ */
 
-/* string handlers */
+typedef struct history
+{
+	unsigned int id_h;
+	char *comms;
+	struct history *prev;
+	struct history *next;
+} history_t;
+
+/**
+ * struct command_s - An structure for each command
+ *
+ * @command: command with arguments.
+ * @next: pointer to next command.
+ */
+typedef struct command_s
+{
+	char **command;
+	struct command_s *next;
+} command_t;
+
+/* Shell functions */
+command_t **_prompt(char *, char *);
+int _fork(char *, command_t *, char *, char **);
+int _stat(char *, char *);
+int _exec(char *, char **, char **);
+
+/* Utilities */
+char *read_line(void);
+
+size_t _strlen(char *str);
+command_t *_parser_cmd(char *, char *);
+size_t _parser_arg(char *, char **, size_t *);
+void print_char_pointer_arr(char **, size_t);
+int add_nodeint(history_t **head, char *str);
+char *_strdup(char *str);
+void free_listint(history_t *head);
+void print_listint(const history_t *);
+char *find_path(char **);
+char *_strstr(char *haystack, char *needle);
+void print_env(char **);
+char *_which(char *p_rec, char *first_arg);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 int _strcmp(char *s1, char *s2);
-int _strlen(char *s);
-int _strncmp(char *s1, char *s2, int n);
-char *_strdup(char *s);
-char *_strchr(char *s, char c);
+void _exit_func(char **);
+int verif_built_comm(char *str, char **env);
 
-void execution(char *cp, char **cmd);
-char *find_path(void);
+/* Error handler */
+void error_handler(char *, int);
+void error_handler_set_default(int, char *);
 
-/* helper function for efficient free */
-void free_buffers(char **buf);
+/* Command Utilities */
+command_t *new_cmd_node(char *);
+void add_tok_to_cmd(char *, command_t *, size_t, char *);
 
-struct builtin
-{
-	char *env;
-	char *exit;
-} builtin;
-
-struct info
-{
-	int final_exit;
-	int ln_count;
-} info;
-
-struct flags
-{
-	bool interactive;
-} flags;
-
-#endif /* SHELL_H */
+#endif
